2e3d5c013b7052c1b9552f9275426d1994aed80b
Your Name <you@example.com>
2024-10-13 15:17:33 +0800

    a

--- Source/ue544_blankkkkkkkkkk/KnowledgeNode.cpp
+++ Source/ue544_blankkkkkkkkkk/KnowledgeNode.cpp
@@ -16,7 +16,7 @@
 AKnowledgeNode::AKnowledgeNode()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 	if (0)
 	{
 		AutoPossessPlayer = EAutoReceiveInput::Player0;
