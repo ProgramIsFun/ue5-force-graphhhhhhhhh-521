200aab3b28729326f4cd8fc40f16a71e1c0d3043
nameeeeeee <nameeeeeeeee@example.com>
2024-10-31 14:34:21 +0800

    a

---
+++ Source/ue544_blankkkkkkkkkk/MyActortest77.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActortest77.h"
+
+// Sets default values
+AMyActortest77::AMyActortest77()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActortest77::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActortest77::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
